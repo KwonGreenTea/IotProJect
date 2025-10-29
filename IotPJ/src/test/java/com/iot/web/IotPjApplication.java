e1c07707c0e109544ba230e2078800f695f31a7e
kgt <bnzzzang@gmail.com>
2025-10-29 23:52:57 +0900

    init file

--- IotPJ/src/test/java/com/iot/web/IotPjApplication.java
+++ IotPJ/src/test/java/com/iot/web/IotPjApplication.java
@@ -1,10 +1,10 @@
-package com.mokcoding.mysite;
+package com.iot.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyWebSiteApplicationTests {
+class IotPjApplication {
 
 	@Test
 	void contextLoads() {
