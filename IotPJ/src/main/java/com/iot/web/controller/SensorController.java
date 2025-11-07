b4a77de027162c29e732f8fbcbb1fb48b266ce03
kgt <bnzzzang@gmail.com>
2025-11-08 02:43:31 +0900

    Post->Get

--- IotPJ/src/main/java/com/iot/web/controller/SensorController.java
+++ IotPJ/src/main/java/com/iot/web/controller/SensorController.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -32,7 +31,7 @@ public class SensorController {
     }
 
     // 데이터 수신 시마다 웹으로 푸시
-    @PostMapping("/api/data")
+    @GetMapping("/api/data")
     public ResponseEntity<String> receiveDataPost(@RequestBody Map<String, Object> jsonData) {
     	log.info("receiveDataPost()");
 		
