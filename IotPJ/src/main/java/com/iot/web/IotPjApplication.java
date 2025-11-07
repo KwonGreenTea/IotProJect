249ada1d2b2e53d17211f696ae3722b8171aba22
kgt <bnzzzang@gmail.com>
2025-11-07 23:48:19 +0900

    서버 세팅 수정

--- IotPJ/src/main/java/com/iot/web/IotPjApplication.java
+++ IotPJ/src/main/java/com/iot/web/IotPjApplication.java
@@ -2,10 +2,16 @@ package com.iot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class IotPjApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(IotPjApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IotPjApplication.class, args);
